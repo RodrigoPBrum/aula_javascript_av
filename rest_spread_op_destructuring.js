5510d8c8ffcd2976b24dfaa59be2d4f3e09299d2
BrumPRodrigo <61873829+BrumPRodrigo@users.noreply.github.com>
2020-06-25 10:18:29 -0300

    destructuring

--- rest_spread_op_destructuring.js
+++ rest_spread_op_destructuring.js
@@ -116,3 +116,83 @@ const obj2 = {...obj, subObj: { ...obj.subObj } };
 obj2.subObj.test = 456;
 //altera a propridade do objeto 1
 console.log(obj);
+
+
+//forma antiga
+var arr = ['Apple','Banana','Orange', ['tomato']];
+
+var apple = arr[0];
+var banana = arr[1];
+var orange = arr[2];
+var tomato = arr[3][0];
+
+//destructuring Assignment
+
+var [apple2, banana2, Orange2, [tomato2]] = ['Apple','Banana','Orange',  ['tomato']];
+
+console.log(tomato2);
+
+//forma antiga 
+
+var obj = {
+    name: 'Rodrigo',
+    props: {
+        age: 30,
+        favoriteColors: ['black', 'blue']
+    }
+};
+
+var age = obj.props.age;
+var color1 = obj.props.favoriteColors[0];
+
+var name = obj.name;
+
+//destructuring Assignment - obj
+
+var { name } = obj;
+
+console.log(name);
+
+//destructuring Assignment - obj literal
+
+var [apple2] = arr;
+var { 
+    props: { age: age2, favoriteColors: [color1, color2] } 
+} = obj;
+
+console.log(age);
+
+var { name: name2 } = obj;
+
+console.log(name2);
+
+console.log(color1);
+
+
+var arr = [{name: 'apple', type: 'fruit'}];
+
+var fruit1 = arr[0].name;
+
+var [{ name: fruitName }] = arr;
+
+console.log(fruitName);
+
+//function
+
+function sum(arr) {
+    return arr[0] + arr[1];
+};
+
+
+//destructuring in function
+function sum([a, b] = [0, 0]) {
+    return a + b;
+};
+
+console.log(sum(5, 5));
+
+function sum({ a, b }) {
+    return a + b;
+};
+
+console.log(sum({ a:5, b:5 }));
