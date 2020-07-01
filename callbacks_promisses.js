67da742ef4e7c70fc9c8297818ad3a773b0f8f82
BrumPRodrigo <61873829+BrumPRodrigo@users.noreply.github.com>
2020-07-01 15:26:06 -0300

    callbacks

---
+++ callbacks_promisses.js
@@ -0,0 +1,62 @@
+// //callback
+
+// function doSomething(callback) {
+//     setTimeout(function () {
+//         //did something
+//         callback('First data');
+//     }, 1000);
+// }
+
+// function doOtherThing(callback) {
+//     setTimeout(function () {
+//         //did other thing
+//         callback('Second data');
+//     }, 1000);
+// }
+
+// function doAll() {
+//     try {
+//         doSomething(function (data) {
+//             var processedData = data.split('');
+
+//             try {
+//                 doOtherThing(function (data2) {
+//                     var processedData2 = data2.split('');
+
+//                     try {
+//                         setTimeout(function () {
+//                             console.log(processedData, processedData2);
+//                         }, 1000);
+//                     } catch (err) {
+//                         // handle error
+//                     }
+//                 });
+//             } catch (err) {
+//                 // handle error
+//             }
+//         });
+//     } catch (err) {
+//         // handle error
+
+//     }
+// }
+
+// doAll();
+
+//promisses
+
+const doSomethingPromise  = new Promise ((resolve, reject) => {
+    setTimeout(function() {
+    //did something
+    resolve('First data');
+    }, 1000);
+});
+
+const doOtherThingPromise  = new Promise ((resolve, reject) => {
+    setTimeout(function() {
+    //did something
+    resolve('Second data');
+    }, 1000);
+});
+
+console.log(doSomethingPromise)
