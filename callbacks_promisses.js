382e7923b91409fdc64622bb45c2a3b45b9583f7
BrumPRodrigo <61873829+BrumPRodrigo@users.noreply.github.com>
2020-07-02 11:10:15 -0300

    promisses

--- callbacks_promisses.js
+++ callbacks_promisses.js
@@ -43,20 +43,71 @@
 
 // doAll();
 
-//promisses
+//Promisses
 
-const doSomethingPromise  = new Promise ((resolve, reject) => {
+// const doSomethingPromise  = () => 
+// new Promise ((resolve, reject) => {
+//     setTimeout(function() {
+//     //did something
+//     resolve('First data');
+//     }, 1000);
+// });
+
+// const doOtherThingPromise  = () => 
+// new Promise ((resolve, reject) => {
+//     // throw new Error("Something wrong");
+//     setTimeout(function() {
+//     //did something
+//     resolve('First data');
+//     }, 1000);
+// });
+
+// doSomethingPromise()
+//     .then(data => { 
+//         console.log(data); 
+//         return doOtherThingPromise();
+//     })
+//     .then(data2 => console.log(data2))
+//     .catch(error => console.log('Ops', error));
+
+// //Pending
+// //Fulfilled
+// //Rejected
+
+
+const doSomethingPromise  = () => 
+new Promise ((resolve, reject) => {
     setTimeout(function() {
     //did something
     resolve('First data');
-    }, 1000);
+    }, 1500);
 });
 
-const doOtherThingPromise  = new Promise ((resolve, reject) => {
+const doOtherThingPromise  = () => 
+new Promise ((resolve, reject) => {
+    // throw new Error('Oops');
     setTimeout(function() {
     //did something
     resolve('Second data');
     }, 1000);
 });
 
-console.log(doSomethingPromise)
+Promise.race([doSomethingPromise(), doOtherThingPromise()]).then(data => {
+    console.log(data);
+});
+
+
+// Promise.all([doSomethingPromise(), doOtherThingPromise()])
+//     .then(data => {
+//         console.log(data[0].split(''));
+//         console.log(data[1].split(''));
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// doSomethingPromise()
+//     .then(data => { 
+//         console.log(data.split('')); 
+//         return doOtherThingPromise();
+//     })
+//     .then(data2 => console.log(data2.split('')));
